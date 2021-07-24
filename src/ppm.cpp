5d32b46c136debbf4dd31a4d5e530cac02c5c282
tartaropoulos <rangel.stanev@gmail.com>
2021-07-24 18:49:41 +0300

    fix bug with reading header from file

--- src/ppm.cpp
+++ src/ppm.cpp
@@ -155,6 +155,9 @@ std::istream& PPM::operator>>(std::istream& is, PPM::PPMHeader& header)
           header.m_height >> 
           header.m_maxValueColor;
 
+    [[maybe_unused]]
+    char unused{static_cast<char>( is.get() )};
+
     return is;
 }
 
