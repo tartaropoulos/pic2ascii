ef7f741b11a649289c578d3581d87ed01a7cfbf2
tartaropoulos <rangel.stanev@gmail.com>
2021-07-26 23:27:22 +0300

    code formatting

--- include/ascii.h
+++ include/ascii.h
@@ -25,14 +25,14 @@ namespace ASCII
     class Converter
     {
     private:
-        const std::vector<char>  m_asciiCharacters{'M', 'b', '5', 'f', 'l', '(', ')', '=', '-', ' '};
-        const int                m_length{ static_cast<int>( m_asciiCharacters.size() - 1 ) };
+        const std::vector<char>  mc_asciiCharacters{'M', 'b', '5', 'f', 'l', '(', ')', '=', '-', ' '};
+        const int                mc_length{ static_cast<int>( mc_asciiCharacters.size() - 1 ) };
 
         // Luminance values (Y in CIE xyY) for sRGB. 
         // Taken from http://www.brucelindbloom.com/index.html?WorkingSpaceInfo.html
-        const float              m_rY{0.2126f};
-        const float              m_gY{0.7152f};
-        const float              m_bY{0.0722f};
+        const float              mc_rY{0.2126f};
+        const float              mc_gY{0.7152f};
+        const float              mc_bY{0.0722f};
 
         std::vector<char>        m_result;
         int                      m_imageWidth;
@@ -47,19 +47,19 @@ namespace ASCII
 
             m_result.resize(newResultSize);
 
-            int rangePerAsciiCharacter{ image.getMaxValueColor() / m_length };
+            int rangePerAsciiCharacter{ image.getMaxValueColor() / mc_length };
 
             for (int y{0}; y < m_imageHeight; ++y)
             {
                 for (int x{0}; x < m_imageWidth; ++x)
                 {
                     auto color{ image.getColor(x, y) };
-                    int  grayR{ static_cast<int>( color.getR() * m_rY ) };
-                    int  grayG{ static_cast<int>( color.getG() * m_gY ) };
-                    int  grayB{ static_cast<int>( color.getB() * m_bY ) };
+                    int  grayR{ static_cast<int>( color.getR() * mc_rY ) };
+                    int  grayG{ static_cast<int>( color.getG() * mc_gY ) };
+                    int  grayB{ static_cast<int>( color.getB() * mc_bY ) };
                     int   gray{ grayR + grayG + grayB };
 
-                    m_result.at( x + y * m_imageWidth + y ) = m_asciiCharacters.at( gray / rangePerAsciiCharacter );
+                    m_result.at( x + y * m_imageWidth + y ) = mc_asciiCharacters.at( gray / rangePerAsciiCharacter );
                 }
 
                 m_result.at( m_imageWidth + y * m_imageWidth + y ) = '\n';
