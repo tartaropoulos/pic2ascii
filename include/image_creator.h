e141e8e77bcc8c6697105120c7017d01a609c248
tartaropoulos <rangel.stanev@gmail.com>
2021-08-04 12:27:33 +0300

    move createImage method in cpp file

--- include/image_creator.h
+++ include/image_creator.h
@@ -23,24 +23,7 @@ private:
     };
 
 public:
-    std::optional< std::unique_ptr<Image::ImageBase> > createImage(const std::filesystem::path& filepath)
-    {
-        if ( !filepath.has_extension() )
-        {
-            return {};
-        }
-
-        std::string extension{ filepath.extension().string() };
-
-        // If file not exists or had wrong extension
-        if ( !std::filesystem::exists(filepath) ||
-             !mc_createImageFunctions.contains(extension) )
-        {
-            return {};
-        }
-
-        return mc_createImageFunctions.at(extension)(filepath);
-    }
+    std::optional< std::unique_ptr<Image::ImageBase> > createImage(const std::filesystem::path& filepath);
 };
 
 #endif // IMAGE_CREATOR_H
---
+++ src/image_creator.cpp
@@ -0,0 +1,20 @@
+#include "image_creator.h"
+
+std::optional< std::unique_ptr<Image::ImageBase> > ImageCreator::createImage(const std::filesystem::path& filepath)
+{
+    if ( !filepath.has_extension() )
+    {
+        return {};
+    }
+
+    std::string extension{ filepath.extension().string() };
+
+    // If file not exists or had wrong extension
+    if ( !std::filesystem::exists(filepath) ||
+            !mc_createImageFunctions.contains(extension) )
+    {
+        return {};
+    }
+
+    return mc_createImageFunctions.at(extension)(filepath);
+}
