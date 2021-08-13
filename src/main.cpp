84746ac634e61d0c8d5a30e6cf1888a5d5d59d1e
tartaropoulos <rangel.stanev@gmail.com>
2021-08-13 13:35:55 +0300

    define main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,72 @@
 #include <iostream>
 
+#include "ascii.h"
+#include "args.h"
+#include "image_creator.h"
+
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	if (argc < 2)
+	{
+		std::cout << "Program work only with arguments!" << std::endl; 
+		Args::printHelp();
+		return EXIT_FAILURE;
+	}
+
+	std::vector<std::string> args{argv + 1, argv + argc};
+
+	Args resultArgs{args};
+
+	if ( !resultArgs.hasFilepath() ||
+		 resultArgs.hasHelp())
+	{
+		resultArgs.printHelp();
+		return EXIT_SUCCESS;
+	}
+
+	std::filesystem::path imageFilepath{ resultArgs.getFilepath().value() };
+	ImageCreator creator;
+	std::optional result{ creator.createImage(imageFilepath) };
+
+	if (!result.has_value())
+	{
+		std::cout << "Can't find image!" << std::endl;
+		resultArgs.printHelp();
+		return EXIT_FAILURE;
+	}
+
+	std::unique_ptr<Image::ImageBase> resultImage;
+	resultImage = std::move(result.value());
+
+	bool hasWidth{ resultArgs.hasWidth() };
+	bool hasHeight{ resultArgs.hasHeight() };
+
+	if ( hasWidth &&
+		 hasHeight )
+	{
+		resultImage->resize( *resultArgs.getWidth(), *resultArgs.getHeight() );
+	}
+	else if ( hasWidth )
+	{
+		resultImage->resize( *resultArgs.getWidth() );
+	}
+	else if ( hasHeight )
+	{
+		resultImage->resize();
+		resultImage->resize( resultImage->getWidth(), *resultArgs.getHeight() );
+	}
+	else
+	{
+		resultImage->resize();
+	}
+
+	ASCII::Converter converter;
+
+	converter.convert(resultImage);
+	converter.print();
+
+	if ( resultArgs.hasSavepath() )
+	{
+		converter.save( *resultArgs.getSavepath() );
+	}
 }
